cc87d66669759bdc950dba2325c1da68667a1ba1
Lautaro Fernandez Szutner <111034147+Lau-szutner@users.noreply.github.com>
2025-01-23 05:39:23 +0000

    agrego efecto de transparencia

--- cash-flow/src/app/components/Navbar.jsx
+++ cash-flow/src/app/components/Navbar.jsx
@@ -1,19 +1,21 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 export const Navbar = () => {
   return (
-    <div className='h-20 bg-gray-500 flex justify-between items-center p-5'>
-        <h1>HELLO</h1>
-        
-        <ul className='flex flex-col lg:flex-row gap-10'>
-            <li>
-                <Link href="/Gastos">Gastos</Link>
-            </li>
-            <li>analisis</li>
-            <li>planes</li>
-            <li>educacion</li>
-        </ul>
+    <div className="h-20 glass flex justify-between items-center p-5">
+      <h1>Cash Flow</h1>
+
+      <ul className="flex flex-col lg:flex-row gap-10">
+        <li>
+          <Link href="/Gastos" className="btn">
+            Gastos
+          </Link>
+        </li>
+        <li className="btn">Analisis</li>
+        <li className="btn">Planes</li>
+        <li className="btn">Educacion</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
