3dd64870344fdb140e324901367e07d17acea8e6
Lautaro Fernandez Szutner <111034147+Lau-szutner@users.noreply.github.com>
2025-03-03 08:43:30 +0000

    NewSpend dd

--- frontend/src/app/components/Balance.jsx
+++ frontend/src/app/components/Balance.jsx
@@ -1,6 +1,16 @@
+'use client';
+
 import React from 'react';
 
+import { useState } from 'react';
+import Newspend from './Newspend';
+
 export const Balance = ({ balance, saving, investing }) => {
+  // const [newSpend, setNewSpend] = useState({ category: '', amount: 0 });
+  const [newSpend, setNewSpend] = useState(false);
+  let handleAddSpend = () => {
+    setNewSpend(!newSpend);
+  };
   return (
     <div className="w-full fixed-top ">
       <div className="bg-gray-500 p-5 rounded-lg m-5">
@@ -12,10 +22,16 @@ export const Balance = ({ balance, saving, investing }) => {
           {/* <li>{`Invertido: ${investing}`}</li> */}
         </ul>
         <div className="flex justify-between gap-2 mt-2">
-          <button className="py-1 px-10 bg-blue-400 rounded w-full">+</button>
+          <button
+            className="py-1 px-10 bg-blue-400 rounded w-full"
+            onClick={handleAddSpend}
+          >
+            +
+          </button>
           <button className="py-1 px-10 bg-red-400 rounded w-full">-</button>
         </div>
       </div>
+      {newSpend && <Newspend />}
     </div>
   );
 };
