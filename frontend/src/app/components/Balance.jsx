d814561221bbc88ac4f82b903bb7645279ee26cf
Lau-szutner <lautaroszutner@gmail.com>
2025-03-09 10:32:58 -0300

    agrego css

--- frontend/src/app/components/Balance.jsx
+++ frontend/src/app/components/Balance.jsx
@@ -18,32 +18,37 @@ export const Balance = ({ balance, saving, monthly }) => {
   };
   return (
     <div className="w-full">
-      <div className="bg-bgComponents p-5 rounded-lg m-5">
+      <div className="bg-bgComponents p-5 rounded-lg m-5 text-2xl">
         <div className="flex flex-col gap-2 width-full">
-          <div className="width-full flex justify-between">
-            <p className="font-bold">Income:</p>
-            <p className="text-green-400">{balance}</p>
-          </div>
-          <div className="width-full flex justify-between">
-            <p className="font-bold">Savings:</p>
-            <p className="text-yellow-600">{saving}</p>
+          <div className="width-full flex justify-between  border-b border-b-whiteText">
+            <p className="font-bold ">Income:</p>
+            <p className="text-greenIn">{balance}</p>
           </div>
 
+          <div className="width-full flex justify-between  border-b border-b-whiteText">
+            <p className="font-bold ">Income:</p>
+            <p className="text-[#FFAA00]">{saving}</p>
+          </div>
+
+          <div className="width-full flex justify-between  border-b border-b-whiteText">
+            <p className="font-bold ">Income:</p>
+            <p className="text-redSpend">{saving}</p>
+          </div>
           {/* <li>{`Ahorro: ${saving}`}</li>
           <li>{`Gasto total mes: ${monthly}`}</li> */}
         </div>
         <div className="flex justify-between gap-2 mt-2">
           <button
-            className="py-1 bg-blue-400 rounded w-full"
+            className="py-1 bg-redSpend rounded w-full drop-shadow-[3px_4px_4px_rgba(0,0,0,0.45)]"
             onClick={handleNewIncome}
           >
-            +
+            -
           </button>
           <button
-            className="py-1 bg-red-400 rounded w-full"
+            className="py-1 bg-greenIn rounded w-full text-3xl "
             onClick={handleAddSpend}
           >
-            -
+            +
           </button>
         </div>
       </div>
