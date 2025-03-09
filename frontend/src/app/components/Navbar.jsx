7540efb6ea4b55f66c8e9da0459b3870f4a65ab4
Lautaro Fernandez Szutner <111034147+Lau-szutner@users.noreply.github.com>
2025-03-09 01:20:27 +0000

    person navbar

--- frontend/src/app/components/Navbar.jsx
+++ frontend/src/app/components/Navbar.jsx
@@ -3,11 +3,22 @@ import React from 'react';
 
 export const Navbar = () => {
   return (
-    <nav className="h-15 bg-gray-600 flex justify-between items-center p-3 flex-col lg:flex-row sticky top-0 w-full z-10">
-      <h1>Money Mind</h1>
+    <nav className="h-15 bg-neutral-900 flex justify-between items-center p-3 flex-col lg:flex-row sticky top-0 w-full z-10">
+      <div className="flex justify-between items-center w-full px-4">
+        <div className="flex flex-col justify-between w-8 h-6 cursor-pointer">
+          <div className="w-full h-1 bg-white mb-1"></div>
+          <div className="w-full h-1 bg-white mb-1"></div>
+          <div className="w-full h-1 bg-white mb-1"></div>
+        </div>
 
-      <ul className="flex flex-col lg:flex-row gap-10 invisible lg:visible">
-        {/* Cada elemento se vuelve invisible en pantallas pequeñas */}
+        <div className="text-white">
+          <h1 className="text-lg">John Doe</h1>
+          <h1 className="text-lg">John Doe</h1>
+        </div>
+      </div>
+
+      {/* <ul className="flex flex-col lg:flex-row gap-10 invisible lg:visible">
+        Cada elemento se vuelve invisible en pantallas pequeñas
         <li className="invisible lg:visible">
           <Link href="/Gastos" className="btn">
             Gastos
@@ -16,7 +27,7 @@ export const Navbar = () => {
         <li className="btn invisible lg:visible">Analisis</li>
         <li className="btn invisible lg:visible">Planes</li>
         <li className="btn invisible lg:visible">Educacion</li>
-      </ul>
+      </ul> */}
     </nav>
   );
 };
