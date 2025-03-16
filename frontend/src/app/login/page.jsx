067bc3eb54f9d6e70971ee312a14f4d006e3d7a7
Lau-szutner <lautaroszutner@gmail.com>
2025-03-15 23:19:39 -0300

    poco avance en login

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -1,10 +1,32 @@
+'use client';
 import React from 'react';
+import { useState } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleRegister() {
+    e.preventDefault();
+    // let email = e.target.email.value;
+    // let password = e.target.password.value;
+    // let response = await fetch('http:localhost:3001/createAccount', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ email, password }),
+    // });
+    // let data = await response.json();
+    // console.log(data);
+    console.log(email);
+    console.log(password);
+  }
+
   return (
     <div className="flex flex-col bg-background h-screen w-full">
       <form
-        action=""
+        // action="POST"
         className="bg-bgComponents rounded-lg text-2xl m-10 p-10 "
       >
         <h1 className="mb-4 text-center">Create Account</h1>
@@ -15,8 +37,9 @@ export default function Login() {
             type="text"
             name="email"
             id="email"
-            className="p-2 border rounded"
+            className="p-2 border rounded text-black"
             placeholder="EmailExample@hotmail.com"
+            onChange={(e) => setEmail(e.target.value)}
           />
         </label>
 
@@ -26,13 +49,16 @@ export default function Login() {
             type="password"
             name="password"
             id="password"
-            className="p-2 border rounded"
+            className="p-2 border rounded text-black"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </label>
 
         <button
           type="submit"
           className="p-2 bg-blue-500 text-white rounded w-full"
+          onClick={() => handleRegister()}
         >
           Submit
         </button>
