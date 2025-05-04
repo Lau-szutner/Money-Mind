468e256f7110d005084791484a497ad037be87d4
lau-szutner <lautaroszutner@gmail.com>
2025-05-03 22:35:40 -0300

    sumo componente

--- frontend/src/app/education/components/EducationTabBar.tsx
+++ frontend/src/app/education/components/EducationTabBar.tsx
@@ -3,6 +3,11 @@ const EducationTabBar = () => {
     <div>
       <div>
         <h2>Education</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos sunt
+          assumenda quidem porro explicabo illo reiciendis ullam voluptas esse
+          consequatur?
+        </p>
       </div>
     </div>
   );
--- frontend/src/app/education/page.tsx
+++ frontend/src/app/education/page.tsx
@@ -1,9 +1,12 @@
 // app/education/page.tsx
+
+import EducationTabBar from './components/EducationTabBar';
 export default function EducationPage() {
   return (
     <main className="min-h-screen p-8 bg-neutral-950 text-white">
       <h1 className="text-3xl font-bold mb-4">Educación</h1>
       <p>Información sobre tu formación académica, cursos, etc.</p>
+      <EducationTabBar />
     </main>
   );
 }
