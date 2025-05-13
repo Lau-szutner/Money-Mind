f982bd5c483f40791a132e76f1bd61811000a6c9
lau-szutner <lautaroszutner@gmail.com>
2025-05-12 22:16:31 -0300

    footer

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import Link from 'next/link';
 
 function Footer() {
   return (
     <div className="h-10 bg-gray-600 w-full flex justify-between items-center p-5 flex-col lg:flex-row bottom-0 ">
       <ul className="flex gap-10">
-        <Link href="/transactions" className="btn">
-          Transacciones
-        </Link>
+        <li>Transactions</li>
         <li>Home</li>
         <li>Gastos</li>
         <li>Fechas</li>
