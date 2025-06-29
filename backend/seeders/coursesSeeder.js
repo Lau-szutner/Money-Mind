f3fa01e0b76333623b929ad73b8f0b9762e54dc4
lau-szutner <lautaroszutner@gmail.com>
2025-06-28 21:11:13 -0300

    agrego seeder y componente de course

--- backend/seeders/coursesSeeder.js
+++ backend/seeders/coursesSeeder.js
@@ -8,12 +8,27 @@ async function coursesSeeder() {
         author: 'Sarah Thompson',
         description:
           'Learn how to take control of your personal finances with practical budgeting techniques. This course provides step-by-step guidance to track your expenses, reduce unnecessary costs.',
-        price: 1,
+        price: 19.99,
       },
       {
-        title: 'Curso avanzado',
-        author: 'Jane Doe',
-        description: 'Un curso avanzado de ejemplo',
+        title: 'Big Goals Without Stress',
+        author: 'David R. Morales',
+        description:
+          "Whether you're saving for a car, vacation, or emergency fund, this course will help you build a personalized saving strategy. Understand short vs long-term goals, automate savings",
+        price: 25.5,
+      },
+      {
+        title: 'Credit Cards',
+        author: 'Michelle K. Reeves',
+        description:
+          'Discover how to use credit cards responsibly, avoid common traps, and increase your credit score. This course explains how interest, limits, and credit history work',
+        price: 12.5,
+      },
+      {
+        title: 'Big Goals Without Stress',
+        author: 'David R. Morales',
+        description:
+          "Whether you're saving for a car, vacation, or emergency fund, this course will help you build a personalized saving strategy. Understand short vs long-term goals, automate savings",
         price: 25.5,
       },
     ]);
