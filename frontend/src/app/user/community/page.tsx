321eeb01ab20ef9dc503343206082155e6c5ebb6
Lau-szutner <lautaroszutner@gmail.com>
2026-05-01 18:39:20 -0300

    elimino codigo sin uso

--- frontend/src/app/user/community/page.tsx
+++ frontend/src/app/user/community/page.tsx
@@ -22,52 +22,12 @@ export default function Community() {
   const [userName, setUserName] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
 
-  // Obtener el nombre del usuario autenticado
-  useEffect(() => {
-    const fetchUserData = async () => {
-      const token = Cookies.get('authToken');
-      if (!token) {
-        console.warn('No token found');
-        return;
-      }
-
-      try {
-        const res = await fetch('/api/user/me', {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        if (res.ok) {
-          const userData = await res.json();
-          setUserName(userData.name);
-        } else {
-          console.warn('No se pudo obtener el usuario');
-        }
-      } catch (err) {
-        console.error('Error al obtener usuario:', err);
-      }
-    };
-
-    fetchUserData();
-  }, []);
-
-  // Fetch posts desde backend al montar el componente
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         const res = await fetch('http://localhost:4000/posts/');
         if (!res.ok) throw new Error('Error al cargar los posts');
         const data = await res.json();
-
-        // const postsFormatted = data.map((post: any) => ({
-        //   id: post.id,
-        //   user: post.User.name,
-        //   title: post.title,
-        //   body: post.body,
-        //   createdAt: post.created_at || post.createdAt || '',
-        // }));
 
         const postsFormatted = data.map((post: any) => {
           const rawDate = post.createdAt;
